74b11e2494fbbcd9c22cb58bf687cff94d2e4230
mateus-code-98 <teu-9@hotmail.com>
2021-11-24 00:03:21 -0300

    asdsad

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,23 +12,25 @@ class UserController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth.jwt', ['except' => ['store','showBarberShops','showOnlyBarberShop']]);
+        $this->middleware('auth.jwt', ['except' => ['store', 'showBarberShops', 'showOnlyBarberShop']]);
     }
 
-    public function ConfereID($id){
+    public function ConfereID($id)
+    {
         $user = JWTAuth::parseToken()->authenticate();
-        if($user->id === $id)return $user;
+        if ($user->id === $id) return $user;
         return false;
     }
 
     public function showBarberShops()
     {
-        $users = User::where('type','barbershop')->with('barbers')->get();
+        $users = User::where('type', 'barbershop')->with('barbers')->get();
         return response()->json($users);
     }
 
-    public function showOnlyBarberShop($id){
-        $users = User::where('type','barbershop')->where('id',$id)->with('barbers')->first();
+    public function showOnlyBarberShop($id)
+    {
+        $users = User::where('type', 'barbershop')->where('id', $id)->with('barbers')->first();
         return response()->json($users);
     }
 
@@ -42,68 +44,67 @@ class UserController extends Controller
         return response()->json($user);
     }
 
-    public function uploadAvatarUsers(Request $request,$id)
+    public function uploadAvatarUsers(Request $request, $id)
     {
-        $user= $this->ConfereID($id);
-        if($user){
+        $user = $this->ConfereID($id);
+        if ($user) {
             $usuario = User::find($id);
-            if($usuario->path_avatar !== null){
+            if ($usuario->path_avatar !== null) {
                 Storage::disk('public')->delete($usuario->path_avatar);
             }
-            $file_name = "avatares/$usuario->type/" .'image'.time().'.png';
+            $file_name = "avatares/$usuario->type/" . 'image' . time() . '.png';
             $usuario->path_avatar = $file_name;
 
             Storage::disk("public")->put($file_name, base64_decode($request->path_avatar));
             $usuario->save();
             return response()->json($usuario);
         }
-        return response()->json(['success'=>false,'message'=> 'Usuário não autorizado'],401);
+        return response()->json(['success' => false, 'message' => 'Usuário não autorizado'], 401);
     }
 
     public function show($id)
     {
-        $user= $this->ConfereID($id);
-        if($user){
+        $user = $this->ConfereID($id);
+        if ($user) {
             $usuario = User::find($id);
             return response()->json($usuario);
         }
-        return response()->json(['success'=>false,'message'=> 'Usuário não autorizado'],401);
+        return response()->json(['success' => false, 'message' => 'Usuário não autorizado'], 401);
     }
 
     public function update(Request $request, $id)
     {
-        $user= $this->ConfereID($id);
-        if($user){
+        $user = $this->ConfereID($id);
+        if ($user) {
             $userData = $request->all();
             $usuario  = User::find($id);
             $usuario->fill($userData);
             $usuario->save();
             return response()->json($usuario);
         }
-        return response()->json(['success'=>false,'message'=> 'Usuário não autorizado'],401);
+        return response()->json(['success' => false, 'message' => 'Usuário não autorizado'], 401);
     }
 
-    public function updatePassword(Request $request,$id){
-        $user= $this->ConfereID($id);
-        if($user){
+    public function updatePassword(Request $request, $id)
+    {
+        $user = $this->ConfereID($id);
+        if ($user) {
             $usuario = User::find($id);
             $usuario->password = bcrypt($request->password);
             $usuario->save();
             return response()->json($usuario);
         }
-        return response()->json(['success'=>false,'message'=> 'Usuário não autorizado'],401);
+        return response()->json(['success' => false, 'message' => 'Usuário não autorizado'], 401);
     }
 
     public function destroy($id)
     {
-        $user= $this->ConfereID($id);
-        if($user){
+        $user = $this->ConfereID($id);
+        if ($user) {
             $usuario = User::find($id);
             $usuario->delete();
             return response()->json($usuario);
         }
-        return response()->json(['success'=>false,'message'=> 'Usuário não autorizado'],401);
+        return response()->json(['success' => false, 'message' => 'Usuário não autorizado'], 401);
     }
-
 }
-
